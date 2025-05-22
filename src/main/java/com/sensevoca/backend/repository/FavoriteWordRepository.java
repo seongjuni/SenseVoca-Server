dc9d4d95999a79aba4a38d081bd3e9a6b139ae89
seongjuni <108966795+seongjuni@users.noreply.github.com>
2025-05-22 17:32:09 +0900

    Create FavoriteWordRepository.java
    즐겨찾기 레포지토리 생성

---
+++ src/main/java/com/sensevoca/backend/repository/FavoriteWordRepository.java
@@ -0,0 +1,9 @@
+package com.sensevoca.backend.repository;
+
+import com.sensevoca.backend.domain.FavoriteWord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoriteWordRepository extends JpaRepository<FavoriteWord, Long>  {
+}
