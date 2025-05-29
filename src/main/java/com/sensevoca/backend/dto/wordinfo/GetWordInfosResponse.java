886b59d599e6e4fd79b1fc7c96d13a5e0012c304
seongjuni <108966795+seongjuni@users.noreply.github.com>
2025-05-29 11:50:23 +0900

    Create GetWordInfosResponse.java
    기본제공단어 리스트 응답 dto 생성

---
+++ src/main/java/com/sensevoca/backend/dto/wordinfo/GetWordInfosResponse.java
@@ -0,0 +1,7 @@
+package com.sensevoca.backend.dto.wordinfo;
+
+public class GetWordInfosResponse {
+    private Long wordId;             // 단어 id
+    private String word;             // 영어 단어
+    private String meaning;          // 뜻
+}
