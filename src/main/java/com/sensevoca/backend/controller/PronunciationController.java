ffcec2975c24fcbfff2d76044d5c423096b67812
suhyeon <102345466+jeong-su-hyeon@users.noreply.github.com>
2025-06-09 18:38:50 +0900

    Update 발음 교정 Controller
    @RequestPrarm -> @RequestPart로 수정

--- src/main/java/com/sensevoca/backend/controller/PronunciationController.java
+++ src/main/java/com/sensevoca/backend/controller/PronunciationController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
@@ -25,9 +22,9 @@ public class PronunciationController {
 
     @PostMapping("")
     public ResponseEntity<ResponseDTO<GetPronunciationResponse>> evaluate_pronunciation(
-            @RequestParam String word,
-            @RequestParam String country,
-            @RequestParam MultipartFile audio)
+            @RequestPart String word,
+            @RequestPart String country,
+            @RequestPart MultipartFile audio)
     {
         try
         {
