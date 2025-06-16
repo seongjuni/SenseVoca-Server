1d3ff8eee62da4cc509babcfd493f2bf4e1540c5
seongjuni <108966795+seongjuni@users.noreply.github.com>
2025-06-16 14:04:38 +0900

    연상 예문 재요청 응답 dto 생성
    연상 예문 재요청 응답 dto 생성

---
+++ src/main/java/com/sensevoca/backend/dto/mywordbook/RegenerateMnemonicExampleResponse.java
@@ -0,0 +1,14 @@
+package com.sensevoca.backend.dto.mywordbook;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegenerateMnemonicExampleResponse {
+    private String word;
+    private String association;    // 니모닉 예문
+    private String imageUrl;       // AI 이미지 URL
+}
